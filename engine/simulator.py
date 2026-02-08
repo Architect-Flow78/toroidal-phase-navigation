f30c7ba1f0825be576615733b3c3815501168313
Nicolae Pascal <pascalnicolae78@gmail.com>
2026-02-08 20:34:49 +0100

    Update simulator.py

--- engine/simulator.py
+++ engine/simulator.py
@@ -1,1 +1,12 @@
+from phase.nullzones import find_null_zones
+from phase.trajectory import propagate
 
+class PhaseSimulator:
+    def __init__(self, bodies):
+        self.bodies = bodies
+
+    def analyze(self, start_point):
+        nulls = find_null_zones(self.bodies)
+        traj = propagate(start_point, self.bodies)
+        return nulls, traj
+      
