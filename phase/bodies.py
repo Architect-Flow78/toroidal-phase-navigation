3072db41e8ce152cf1366053af6ac160ac2bc6ce
Nicolae Pascal <pascalnicolae78@gmail.com>
2026-02-08 20:32:09 +0100

    Update bodies.py

--- phase/bodies.py
+++ phase/bodies.py
@@ -1,1 +1,7 @@
-
+class PhaseBody:
+    def __init__(self, theta, phi, strength, name="body"):
+        self.theta = theta
+        self.phi = phi
+        self.strength = strength
+        self.name = name
+      
