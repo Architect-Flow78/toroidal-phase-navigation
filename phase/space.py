9ab0b6b6806fc991612e1405438c349f53eebeb7
Nicolae Pascal <pascalnicolae78@gmail.com>
2026-02-08 20:31:38 +0100

    Update space.py

--- phase/space.py
+++ phase/space.py
@@ -1,1 +1,15 @@
+from .constants import PI
+
+def wrap(x):
+    return x % PI
+
+
+class PhasePoint:
+    def __init__(self, theta, phi, s=0.0):
+        self.theta = wrap(theta)
+        self.phi = wrap(phi)
+        self.s = s
+
+    def as_tuple(self):
+        return (self.theta, self.phi, self.s)
 
