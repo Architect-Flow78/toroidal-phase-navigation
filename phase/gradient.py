15b44a4bbb3fe9b57150b79eeba8bd5063c1c0f2
Nicolae Pascal <pascalnicolae78@gmail.com>
2026-02-08 20:33:11 +0100

    Update gradient.py

--- phase/gradient.py
+++ phase/gradient.py
@@ -1,1 +1,16 @@
+import numpy as np
+from .space import PhasePoint
+from .tension import phase_tension
+from .constants import EPS
 
+def phase_gradient(point, bodies):
+    base = phase_tension(point, bodies)
+
+    pθ = PhasePoint(point.theta + EPS, point.phi, point.s)
+    pφ = PhasePoint(point.theta, point.phi + EPS, point.s)
+
+    gθ = (phase_tension(pθ, bodies) - base) / EPS
+    gφ = (phase_tension(pφ, bodies) - base) / EPS
+
+    return np.array([gθ, gφ])
+  
